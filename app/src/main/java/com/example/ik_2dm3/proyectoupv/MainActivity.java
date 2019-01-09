c0f254937e1c7237dc32406d0933b46b0106e664
Cajanegra17 <renegado18>
2019-01-09 12:42:05 +0100

    Creado juego puzzle

--- app/src/main/java/com/example/ik_2dm3/proyectoupv/MainActivity.java
+++ app/src/main/java/com/example/ik_2dm3/proyectoupv/MainActivity.java
@@ -17,6 +17,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button pruebas;
     public boolean admin = false;
     private Button  idBtnMainAjustes,idBtnIzquierda, idBtnDerecha, idBtnContinuar, idBtnInicio, idBtnReiniciar;
     private Button btnPopupInicioSi,btnPopupInicioNo,btnPopupInicioContinuar,btnPopupInicioReiniciar;
@@ -38,6 +40,18 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
         inicioPopup = new Dialog(this);
+
+        // borrar
+        pruebas = (Button) findViewById(R.id.button2);
+        pruebas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getBaseContext(), Actividad_70_Puzzle.class);
+                finish();
+                startActivity(i);
+            }
+        });
+
 
         objetoAjustes = new ajustes(getBaseContext());
         Log.d("ajustes","sonido= "+objetoAjustes.sonido+" // musica= "+objetoAjustes.musica+" // mapa="+objetoAjustes.mapa+" // idioma="+objetoAjustes.idioma);
